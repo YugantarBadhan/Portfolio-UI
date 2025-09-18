f8ad7403e046b3c2529a949cd19778b62f240019
yugantar.badhan <yugantar.badhan@wolterskluwer.com>
2025-09-18 13:08:00 +0530

    added dockerfile and upated env.prod file

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,19 @@
-// src/environments/environment.prod.ts
+// // src/environments/environment.prod.ts
+// export const environment = {
+//   production: true,
+//   apiUrl: '/api',
+//   adminToken: process.env['ADMIN_TOKEN'] || 'yugantarportfoliobadhan',
+//   version: '1.0.0',
+//   features: {
+//     enableAnalytics: true,
+//     enableLogging: false,
+//     enableCache: true
+//   }
+// };
+
 export const environment = {
   production: true,
-  apiUrl: '/api',
+  apiUrl: 'https://portfolio-api-production-b9dc.up.railway.app/api',
   adminToken: process.env['ADMIN_TOKEN'] || 'yugantarportfoliobadhan',
   version: '1.0.0',
   features: {
